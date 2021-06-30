c69ec92fe04c954e29f6189195411918a6f02b14
Gerhard <gerhard.putter@yahoo.com>
2021-06-30 12:12:54 +0200

    Final function of scorekeeping app.

---
+++ script.js
@@ -0,0 +1,59 @@
+//Variables//
+
+let winningScore = 10;
+const resetButton = document.querySelector('#reset');
+let isGameOver = false;
+
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+
+//Game Functions//
+
+function updateScores(player, opponent) {
+    if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
+            isGameOver = true;
+            player.display.classList.add('winner');
+            opponent.display.classList.add('loser');
+            player.button.disabled = true;
+            opponent.button.disabled = true;
+        }
+        player.display.textContent = player.score;
+    }
+}
+
+
+//Event Listeners//
+
+p1.button.addEventListener('click', function () {
+    updateScores(p1, p2)
+})
+p2.button.addEventListener('click', function () {
+    updateScores(p2, p1)
+})
+
+
+resetButton.addEventListener('click', reset)
+
+
+//RESET Function//
+
+function reset() {
+    isGameOver = false;
+    for (let p of [p1, p2]) {
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('winner', 'loser');
+        p.button.disabled = false;
+    }
+}
